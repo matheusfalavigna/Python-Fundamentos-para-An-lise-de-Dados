5ffaa1b6b4bd85e92f14aee827e4044fb9cb3547
matheusfalavigna <matheusfalavigna@gmail.com>
2020-02-08 18:58:10 -0300

    Start Repositori

---
+++ Ex.Cap02.Exercicios.py
@@ -0,0 +1,54 @@
+# Exercicio 1
+lista = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+print(lista)
+print("\n")
+
+# Exercicio 2
+lista = ["Pão","Ovo","Açucar","Arroz","Carne"]
+print(lista)
+print("\n")
+
+# Exercicio 3
+string1 = "Hello"
+string2 = "World"
+string3 = string1 + " " + string2
+print(string3)
+print("\n")
+
+# Exercicio 4
+tupla = (1, 2, 2, 3, 4, 4, 4, 5)
+print(tupla.count(4))
+print("\n")
+
+# Exercicio 5
+dicionario = {}
+print(dicionario)
+print("\n")
+
+# Exercicio 6
+dicionario = {"Matheus":19, "Thiago":19, "Henrique":19}
+print(dicionario)
+print("\n")
+
+# Exercicio 7
+dicionario = {"Matheus":19, "Thiago":19, "Henrique":19}
+dicionario["Pedro"] = 21
+print(dicionario)
+print("\n")
+
+# Exercicio 8
+dicionario ={'Chave 1':"Dgoberto",'Chave 2':[3,2],"Chave 3":100}
+print(dicionario)
+print("\n")
+
+# Exercicio 9
+lista = ["Alouuu",(2,4),{"Arroz":5.90,"Feijão":4.30},101.98]
+print(lista)
+print("\n")
+
+# Exercicio 10
+frase = 'Cientista de Dados é o profissional mais sexy do século XXI'
+print(frase[0:18])
+print("\n")
+
+
